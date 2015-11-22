11030559b1e7b00e766224368e94a1b089e253be
Dan Rice <dan@zoombody.com>
2015-11-22 14:20:31 -0500

    Move new app's files to the root

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
+app_name=test-app
 version=$(echo $release_link | grep -oE '[^\/]+$')
 branch=v$version
 gem install rails -v $version
 git checkout --orphan $branch
 git rm -rf .
-rails new test-app
+rails new $app_name
+mv $app_name/* .
+mv $app_name/.* .
+rmdir $app_name
 git add .
-git commit -m '`rails new test-app`'
+git commit -m '`rails new $app_name`'
 git remote set-url --push origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/dnrce/railsnew.git
 git push origin $branch
