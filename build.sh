f21475e0aae9bff2e19c3cebaf75bc8eb3a08099
Dan Rice <dan@zoombody.com>
2015-11-22 12:29:42 -0500

    Initial build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+version=$(echo $release_link | grep -oE '[^\/]+$')
+branch=v$version
+gem install rails -v $version
+git checkout --orphan $branch
+git rm -rf .
+rails new test-app
+git add .
+git commit -m '`rails new test-app`'
+git push origin $branch
