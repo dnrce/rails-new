5e098c24928b23e652bae2abbccccde2aa6bf213
Dan Rice <dan@zoombody.com>
2016-04-29 23:34:54 -0400

    Fix branch checkout

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ version=$(echo $release_link | grep -oE '[^\/]+$')
 branch=$(echo $version | grep -oE '^[0-9]+\.[0-9]+')
 
 # Checkout existing branch and empty it, or create an empty branch
-git checkout -b $branch || git checkout --orphan $branch
+git checkout $branch || git checkout --orphan $branch
 GLOBIGNORE=.:..:.git ; git rm -r *
 
 # Install Rails
