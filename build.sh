c3037df9279927d004250f58d358de8b43e92c70
Dan Rice <dan@zoombody.com>
2016-04-30 23:08:53 -0400

    Simplify to folders instead of branches

--- build.sh
+++ build.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 set -ev
 
-# Initialize variables
-app_name=test-app
+# Determine version from build parameters
 version=$(echo $release_link | grep -oE '[^\/]+$')
-branch=$(echo $version | grep -oE '^[0-9]+\.[0-9]+')
-
-# Checkout existing branch and empty it, or create an empty branch
-(git fetch origin $branch && git checkout -b $branch FETCH_HEAD) || git checkout --orphan $branch
-GLOBIGNORE=.:..:.git ; git rm -rf *
 
 # Install Rails
 gem install rails -v $version
 
 # Create a new Rails app in the repository root
-rails new $app_name --skip-bundle
-shopt -s dotglob
-mv $app_name/* .
-rmdir $app_name
+rails new rails-new --skip-bundle
+mv rails-new $version
 
 # Commit and push
-git add .
-git commit -m "New Rails $version app \"$app_name\""
-git tag $version
-git push --tags https://$GITHUB_USER:$GITHUB_TOKEN@github.com/dnrce/rails-new.git $branch 2>&1 | grep -v https
+git add $version
+git commit -m "Rails $version"
+git push https://$GITHUB_USER:$GITHUB_TOKEN@github.com/dnrce/rails-new.git 2>&1 | grep -v https
