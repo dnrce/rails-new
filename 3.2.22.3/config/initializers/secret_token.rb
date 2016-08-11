40194f379a618539e1a93de38a4bb1d674743485
rails-new <rails-new@dnrce.github.io>
2016-08-11 17:34:59 +0000

    Rails 3.2.22.3

---
+++ 3.2.22.3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsNew::Application.config.secret_token = 'b30eef20f8ea780f0f15239b37a6a4d85e2a96da33ce6c51512733a79d62f8e3b759ef256cf77e6b9466a29405d326cef57f3d81605b13a7788b2001b4164cfb'
